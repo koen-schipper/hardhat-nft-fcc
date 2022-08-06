f4b0f6dc17de4540220790ceb230347c6cd779ca
Koen Schipper <koen.schipper@me.com>
2022-08-06 19:00:17 +0200

    update

---
+++ deploy/02-deploy-random-ipfs-nft.js
@@ -0,0 +1,42 @@
+const { network, ethers } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let vrfCoordinatorV2Address, subscriptionId
+
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract(
+            "VRFCoordinatorV2Mock"
+        )
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const tx = await vrfCoordinatorV2Mock.createSubscription()
+        const txReceipt = await tx.wait(1)
+        subscriptionId = txReceipt.events[0].args.subId
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
+    }
+    console.log("----------------------------------------------")
+    const args = [
+        vrfCoordinatorV2Address,
+        subscriptionId,
+        networkConfig[chainId].gasLane,
+        networkConfig[chainId].callbackGasLimit,
+        // tokenUris,
+        networkConfig[chainId].mintFee,
+    ]
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(basicNft.address, args)
+    }
+    console.log("----------------------------------------------")
+}
+
+module.exports.tags = ["all", "randomIpfsNft", "main"]
